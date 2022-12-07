c675fb0b35069144d4e368184352dbd4048765d1
nariman85-creator <horoshiycheloveknariman@yandex.ru>
2022-12-07 17:15:43 +0300

    Change MongoDB-URL

--- backend/utils/db.ts
+++ backend/utils/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 // mongodb+srv://nariman:narik85@cluster0.hsiq3wg.mongodb.net/?retryWrites=true&w=majority
 mongoose
   .connect(
-    "mongodb://localhost:27017/ecommercy"
+    "mongodb+srv://nariman:narik85@cluster0.hsiq3wg.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => console.log("Database connect....!"))
   .catch((e) => console.log(e));
